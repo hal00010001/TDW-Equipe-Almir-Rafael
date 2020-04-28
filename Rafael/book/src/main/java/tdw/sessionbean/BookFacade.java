df175270e7276520fd32be2f83bd9295fdf1fcaf
rafaelmpereira <rafaelmpereira.rmp@gmail.com>
2020-04-28 20:14:26 -0300

    Updating sessionbean

--- Rafael/book/src/main/java/tdw/sessionbean/BookFacade.java
+++ Rafael/book/src/main/java/tdw/sessionbean/BookFacade.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package tdw.sessions;
+package tdw.sessionbean;
 
 import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
