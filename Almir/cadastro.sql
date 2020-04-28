152c3e02a516505595e4ee12d0267dc0660f5953
Almir Loiola Bindo <hal0001@gmail.com>
2020-04-28 07:59:19 -0300

    Create cadastro.sql
    SQL to create the database for the Cadastro Project

---
+++ Almir/cadastro.sql
@@ -0,0 +1,62 @@
+-- phpMyAdmin SQL Dump
+-- version 4.6.6deb5
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost:3306
+-- Generation Time: Apr 28, 2020 at 07:57 AM
+-- Server version: 5.7.29-0ubuntu0.18.04.1
+-- PHP Version: 7.2.24-0ubuntu0.18.04.4
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `crud`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `cadastro`
+--
+
+CREATE TABLE `cadastro` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `nome` varchar(200) NOT NULL,
+  `nascimento` date NOT NULL,
+  `cpf` varchar(20) NOT NULL,
+  `contratacao` date NOT NULL,
+  `cargo` varchar(40) NOT NULL,
+  `departamento` varchar(40) NOT NULL,
+  `salario` double NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `cadastro`
+--
+ALTER TABLE `cadastro`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `id` (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `cadastro`
+--
+ALTER TABLE `cadastro`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
