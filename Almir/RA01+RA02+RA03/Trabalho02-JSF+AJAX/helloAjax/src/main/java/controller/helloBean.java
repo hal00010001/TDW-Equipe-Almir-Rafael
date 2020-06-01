f26fe8bf2b54461d9b059d3f35566fad622c8f2d
Almir Loiola Bindo <hal0001@gmail.com>
2020-06-01 16:20:34 -0300

    Criação das pastas para entrega
    Criação das pastas RA01+RA02+RA03 e para os trabalhos específicos para entrega

---
+++ Almir/RA01+RA02+RA03/Trabalho02-JSF+AJAX/helloAjax/src/main/java/controller/helloBean.java
@@ -0,0 +1,47 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package controller;
+
+import javax.inject.Named;
+import javax.enterprise.context.SessionScoped;
+import java.io.Serializable;
+
+/**
+ *
+ * @author hal
+ */
+@Named(value = "helloBean")
+@SessionScoped
+public class helloBean implements Serializable {
+
+    /**
+     * Creates a new instance of helloBean
+     */
+    
+    private String name;
+    
+    public helloBean() {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+       
+    public String getSayWelcome(){
+        //check if null?
+        if("".equals(name) || name == null){
+            return "";
+        }
+        else {
+            return "Ajax message: Welcome " + name;
+        }
+    }
+    
+}
